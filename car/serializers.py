0566d276b4d0df1c2cf811bce3ba6a780aa3f881
Viacheslav417865 <sfinks417865@gmail.com>
2024-11-29 17:29:39 +0200

    Fix serialization and deserialization functions, update migration

--- car/serializers.py
+++ car/serializers.py
@@ -1,36 +1,47 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import serializers
-from .models import Car
+
+from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField(min_value=0, max_value=2000)
-    is_broken = serializers.BooleanField(default=False)
-    problem_description = serializers.CharField(
-        allow_null=True, required=False
+    horse_powers = serializers.IntegerField(
+        validators=[MaxValueValidator(1914), MinValueValidator(1)]
     )
-
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        return data
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        allow_null=True
+    )
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.manufacturer = validated_data.get(
-            "manufacturer", instance.manufacturer
+            "manufacturer",
+            instance.manufacturer
         )
-        instance.model = validated_data.get(
-            "model", instance.model
+        instance.model = validated_data.get("model", instance.model)
+        instance.horse_powers = validated_data.get(
+            "horse_powers",
+            instance.horse_powers
         )
-        instance.horse_powers = validated_data.get("horse_powers",
-                                                   instance.horse_powers)
-        instance.is_broken = validated_data.get("is_broken",
-                                                instance.is_broken)
+        instance.is_broken = validated_data.get(
+            "is_broken",
+            instance.is_broken
+        )
         instance.problem_description = validated_data.get(
-            "problem_description", instance.problem_description
+            "problem_description",
+            instance.problem_description
         )
         instance.save()
         return instance
+
+    class Meta:
+        model = Car
+        fields = "__all__"
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import io
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 from car.models import Car
 from car.serializers import CarSerializer
 import json
@@ -5,13 +8,18 @@ import json
 
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
-    return json.dumps(serializer.data).encode("utf-8")
+    json = JSONRenderer().render(serializer.data)
+    return json
 
 
 def deserialize_car_object(car_json: str) -> Car:
-    car_data = json.loads(car_json)
-    serializer = CarSerializer(data=car_data)
+    if isinstance(car_json, str):
+        car_json = car_json.encode("utf-8")
+    stream = io.BytesIO(car_json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+
     if serializer.is_valid():
         return serializer.create(serializer.validated_data)
     else:
-        raise ValueError("Invalid data for Car object")
+        raise ValueError(f"Invalid data for Car object: {serializer.errors}")
